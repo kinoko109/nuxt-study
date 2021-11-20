57fe32ec1bb5dd399a2bd3ad8fde26863adefc5e
松岡龍紀 <tatsuaki_matsuoka@bita.jp>
2021-11-20 18:08:37 +0900

    fix: 整形

--- pages/users/_id.vue
+++ pages/users/_id.vue
@@ -6,11 +6,11 @@
 </template>
 
 <script>
-  export default {
-    data() {
-      return {
-        message: '_id.vueを表示'
-      }
+export default {
+  data() {
+    return {
+      message: '_id.vueを表示',
     }
-  }
+  },
+}
 </script>
